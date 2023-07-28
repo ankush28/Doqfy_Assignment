326bc6a04c8f652c8a66a39c8cfa10df4f9e2446
Ankush Choudhary <ankushchoudhary@jklu.edu.in>
2023-07-28 20:24:31 +0530

    Desktop UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BalanceComponent } from './components/balance/balance.component';
+import {MatBadgeModule} from '@angular/material/badge';
+import { PendingActionsComponent } from './components/pending-actions/pending-actions.component';
+import { FrequentlyUsedComponent } from './components/frequently-used/frequently-used.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    BalanceComponent,
+    PendingActionsComponent,
+    FrequentlyUsedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule, MatMenuModule, MatIconModule,MatBadgeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pending-actions/pending-actions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-actions',
+  templateUrl: './pending-actions.component.html',
+  styleUrls: ['./pending-actions.component.scss']
+})
+export class PendingActionsComponent {
+
+}
